ebc363464d7dad8deafc2e886112fbf6cc747e0a
Ranujitha-Sugathadasa <119477432+Ranujitha-Sugathadasa@users.noreply.github.com>
2024-07-15 22:42:18 +0530

    Controllers and models

---
+++ Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace Joke.Models
+{
+	public class Joke
+	{
+		public int Id { get; set; }
+		public string JokeQuestion { get; set; }
+		public string JikeAnswer { get; set; }
+
+	public Joke()
+	{
+
+	}
+	}
+}
