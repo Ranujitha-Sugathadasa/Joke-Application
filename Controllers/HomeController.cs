cad27f390396928ffeadf28064e8a0d5514a2b7e
Ranujitha-Sugathadasa <119477432+Ranujitha-Sugathadasa@users.noreply.github.com>
2024-08-05 23:49:49 +0530

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,15 +8,15 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    // public HomeController(ILogger<HomeController> logger)
-    // {
-    //     _logger = logger;
-    // }
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
 
-    // public IActionResult Index()
-    // {
-    //     return View();
-    // }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
     public IActionResult Privacy()
     {
